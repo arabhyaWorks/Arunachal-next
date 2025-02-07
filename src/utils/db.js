1cef3d32433af4cebb32c0fc72a881f96858b284
Arabhaya <r.arabhaya@gmail.com>
2025-02-07 01:01:03 -0800

    some changes in the db

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,11 +1,20 @@
 import mysql from 'mysql2/promise';
 
 // Database connection configuration
+// const dbConfig = {
+//   host: 'developer.chciq0eeey32.ap-south-1.rds.amazonaws.com',
+//   user: 'dev_arunachal_user',
+//   password: 'JaiHindBharat$$321##',
+//   database: 'db_arunachal_pradesh',
+//   waitForConnections: true,
+//   connectionLimit: 10,  // Maximum connections in the pool
+//   queueLimit: 0,
+// };
 const dbConfig = {
-  host: 'developer.chciq0eeey32.ap-south-1.rds.amazonaws.com',
-  user: 'dev_arunachal_user',
-  password: 'JaiHindBharat$$321##',
-  database: 'db_arunachal_pradesh',
+  host: "10.0.105.19",
+  user: "indi",
+  password: "INdi@#$%^99102%",
+  database: "indigeDB",
   waitForConnections: true,
   connectionLimit: 10,  // Maximum connections in the pool
   queueLimit: 0,
